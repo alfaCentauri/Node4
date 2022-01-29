fcb61df865f3b3233129a6cdcbe1ddbfb5d356ee
Ricardo Presilla <rpresilla@initiumsoft.com>
2022-01-29 14:49:46 -0400

    Update app

--- app.js
+++ app.js
@@ -24,18 +24,17 @@ const req = http.request(options, (res) => {
     console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
     res.setEncoding('utf8');
     res.on('data', (chunk) => {
-    console.log(`BODY: ${chunk}`);
+        console.log(`BODY: ${chunk}`);
     });
     res.on('end', () => {
-    console.log('No more data in response.');
+        console.log('No more data in response.');
     });
-    });
+});
     
-    req.on('error', (e) => {
+req.on('error', (e) => {
     console.error(`problem with request: ${e.message}`);
-    });
-    
-    // Write data to request body
-    req.write(postData);
-    req.end();
+});
+// Write data to request body
+req.write(postData);
+req.end();
     
