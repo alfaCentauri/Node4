88789490b3638d0f2128133b81ad1ca0572e33bf
Ricardo Presilla <rpresilla@initiumsoft.com>
2022-01-29 14:48:15 -0400

    Update app

--- app.js
+++ app.js
@@ -1,4 +1,41 @@
 const libreriaMensajes = require('./libreria');
+const http = require('http');
 
-console.log("Ejemplo #3: Depurando desde nodejs.");
+console.log("Ejemplo #4: Depurando desde nodejs.");
 libreriaMensajes.saludo;
+
+const postData = JSON.stringify({
+    'msg': 'Automovil'
+  });
+
+const options = {
+    hostname: 'www.google.com',
+    port: 80,
+    path: '/upload',
+    method: 'POST',
+    headers: {
+    'Content-Type': 'application/json',
+    'Content-Length': Buffer.byteLength(postData)
+    }
+};
+
+const req = http.request(options, (res) => {
+    console.log(`STATUS: ${res.statusCode}`);
+    console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+    res.setEncoding('utf8');
+    res.on('data', (chunk) => {
+    console.log(`BODY: ${chunk}`);
+    });
+    res.on('end', () => {
+    console.log('No more data in response.');
+    });
+    });
+    
+    req.on('error', (e) => {
+    console.error(`problem with request: ${e.message}`);
+    });
+    
+    // Write data to request body
+    req.write(postData);
+    req.end();
+    
